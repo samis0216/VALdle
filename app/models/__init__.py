e4dae2b23367825e93509af8a34c602e58ea66e6
samis0216 <samisamman26@yahoo.com>
2024-03-26 13:59:58 -0700

    fixing migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from .db import db
 from .user import User
 from .dj import DJ
+from .genre import Genre
 from .db import environment, SCHEMA
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .djs import seed_djs, undeo_djs
+from .djs import seed_djs, undo_djs
 from .genres import seed_genres, undo_genres
 
 from app.models.db import db, environment, SCHEMA
@@ -19,13 +19,18 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_djs()
+        undo_genres()
         undo_users()
     seed_users()
+    seed_genres()
+    seed_djs()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_djs()
+    undo_genres()
     undo_users()
     # Add other undo functions here
