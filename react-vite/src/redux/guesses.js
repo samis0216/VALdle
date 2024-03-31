da74c995d02025c02dacfc8b0f24dae62b115559
samis0216 <samisamman26@yahoo.com>
2024-03-31 13:15:40 -0700

    finished guesses reducer

--- react-vite/src/redux/guesses.js
+++ react-vite/src/redux/guesses.js
@@ -1,5 +1,6 @@
 const POST_GUESS = 'guesses/postGuess'
 const GET_GUESSES = 'guesses/getGuesses'
+const RESET_GUESSES = 'guesses/resetGuesses'
 
 const getGuesses = (guesses) => ({
     type: GET_GUESSES,
@@ -9,6 +10,11 @@ const getGuesses = (guesses) => ({
 const postGuess = (guess) => ({
     type: POST_GUESS,
     guess
+})
+
+const resetGuesses = () => ({
+    type: RESET_GUESSES
+    
 })
 
 export const getGuessesThunk = () => async(dispatch) => {
@@ -30,5 +36,45 @@ export const postGuessThunk = (guess) => async(dispatch) => {
     if (res.ok) {
         const data = res.json()
         dispatch(postGuess(data))
+        return data
     }
 }
+
+export const resetGuessesThunk = (id) => async(dispatch) => {
+    const res = await fetch(`/api/guesses/${id}/reset`, {
+        method: 'DELETE'
+    })
+
+    if (res.ok) {
+        const data = res.json()
+        dispatch(resetGuesses())
+        return data
+    }
+}
+
+const initialState = {}
+
+function guessesReducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_GUESSES: {
+            const newState = {}
+            action.guess.forEach(guess => {
+                newState[guess.id] = guess
+            })
+            return newState
+        }
+        case POST_GUESS: {
+            const newState = {...state}
+            newState[action.guess.id] = action.guess
+            return newState
+        }
+        case RESET_GUESSES: {
+            const newState = {}
+            return newState
+        }
+        default:
+            return state
+    }
+}
+
+export default guessesReducer
