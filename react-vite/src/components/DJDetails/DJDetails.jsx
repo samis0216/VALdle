e84039701b63ad61cb50adc417f1896675430208
samis0216 <samisamman26@yahoo.com>
2024-07-06 16:28:55 -0700

    adding dj details useEffect

--- react-vite/src/components/DJDetails/DJDetails.jsx
+++ react-vite/src/components/DJDetails/DJDetails.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { getOneDJThunk } from "../../redux/djs"
 
 export default function DJDetails({dj}) {
     const dispatch = useDispatch()
     const dj = useSelector((state) => state.dj)
+    useEffect(() => {
+        dispatch(getOneDJThunk(dj.id))
+    }, [dj])
     if (dj) return (
         <div>
             <h1 style={{color: 'white'}}>{dj.stagename}</h1>
