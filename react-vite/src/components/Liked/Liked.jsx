2433f8f6d491b972a06ab300049ed415a94180dd
Sami <samisamman26@yahoo.com>
2024-07-27 13:42:01 -0700

    fixing liked component name

--- react-vite/src/components/Liked/Liked.jsx
+++ react-vite/src/components/Liked/Liked.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { useNavigate } from "react-router-dom";
 
-export default function GuessTable({ target }) {
+export default function Liked() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
