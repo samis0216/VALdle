7577d47c3be7f9a750ca76cd762010b3bcda7b8f
Sami <samisamman26@yahoo.com>
2024-07-27 13:41:30 -0700

    beginning liked component

---
+++ react-vite/src/components/Liked/Liked.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+
+export default function GuessTable({ target }) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+}
