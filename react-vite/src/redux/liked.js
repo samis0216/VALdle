283a09fab1e601c8b4bb41e02038ee3507538caa
samis0216 <samisamman26@yahoo.com>
2024-08-03 17:56:50 -0700

    improved like reducer

---
+++ react-vite/src/redux/liked.js
@@ -0,0 +1,28 @@
+const POST_LIKED = 'liked/postLike'
+const GET_LIKED = 'liked/getLiked'
+const RESET_LIKED = 'liked/resetLiked'
+
+const getLiked = (liked) => ({
+    type: GET_LIKED,
+    liked
+})
+
+const getLikedThunk = () => {
+
+}
+
+const initialState = {}
+
+export default function likedReducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_LIKED: {
+            const newState = {}
+            action.liked.forEach(like => {
+                newState[like.id] = like
+            })
+            return newState
+        }
+        default:
+            return state
+    }
+}
