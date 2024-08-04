c0dea856193e9445117ed5e28594785a68061623
samis0216 <samisamman26@yahoo.com>
2024-08-03 17:49:42 -0700

    liked component updated

--- react-vite/src/components/Liked/Liked.jsx
+++ react-vite/src/components/Liked/Liked.jsx
@@ -6,17 +6,24 @@ import { getAllDJThunk } from "../../redux/djs";
 export default function Liked() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const likedDjs = useSelector((state) => state.liked.djs)
 
     useEffect(()=> {
         dispatch(getGenresThunk())
         dispatch(getAllDJThunk())
+        dispatch(getLikedDJsThunk())
     }, [dispatch])
 
     return (
         <div>
             <h1>Liked</h1>
             <div>
-                <h2>DJs</h2>
+                <div>
+                    <h2>DJs</h2>
+                    {likedDJs && likedDjs.map((dj) => (
+                        <h1>{dj.name}</h1>
+                    ))}
+                </div>
                 <h2>Songs</h2>
                 <h2>Genres</h2>
             </div>
