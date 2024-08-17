42b8337d1680f0bc72d8e4ae1ef8bfc5ff7f5b9a
Sami <samisamman26@yahoo.com>
2024-08-17 11:12:45 -0400

    added liked thunks

--- react-vite/src/redux/liked.js
+++ react-vite/src/redux/liked.js
@@ -4,12 +4,47 @@ const RESET_LIKED = 'liked/resetLiked'
 
 const getLiked = (liked) => ({
     type: GET_LIKED,
-    liked
+
 })
 
 const resetLiked = () => ({
     type: RESET_LIKED,
     liked
+})
+
+const postLiked = (newLiked) => ({
+    type: POST_LIKED,
+    newLiked
+})
+
+export const getLikesThunk = () => async(dispatch) => {
+    const res = await fetch('/api/likes')
+
+    if (res.ok) {
+        const data = res.json()
+        dispatch(getLiked(data))
+        return data
+    }
+}
+
+export const resetLikesThunk = () => async(dispatch) => {
+    const res = await fetch('/api/likes/reset')
+
+    if (res.ok) {
+        const data = res.json()
+        dispatch(resetLiked(data))
+        return data
+    }
+}
+
+export const postLikesThunk = () => async(dispatch) => {
+    const res = await fetch('/api/likes/new')
+
+    if (res.ok) {
+        const data = res.json()
+        dispatch(postLiked(data))
+        return data
+    }
 }
 
 const initialState = {}
