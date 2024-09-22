4e98264c8275a2974aa5ac4503bbf37df8b22b4c
Sami <samisamman26@yahoo.com>
2024-09-21 20:35:36 -0400

    slight css adjustment

--- react-vite/src/components/Welcome/Welcome.jsx
+++ react-vite/src/components/Welcome/Welcome.jsx
@@ -8,7 +8,7 @@ export default function Welcome() {
         <div className="homepageMain">
             <video src={stock_dj} id="video" autoPlay loop muted></video>
             <div className="content">
-                <h1 style={{ color: "rgb(250, 246, 0)", marginBottom: '0', fontSize: '40px'}}>Welcome to DJdle!</h1>
+                <h1 style={{ color: "rgb(250, 246, 0)", marginBottom: '10', fontSize: '40px'}}>Welcome to DJdle!</h1>
                 <p className='subtitle' style={{ color: "white" }}>Test your EDM knowledge. Discover new artists.</p>
                 <div>
                     <button onClick={() => navigate('/login')} className="homepageButton">Get Started</button>
