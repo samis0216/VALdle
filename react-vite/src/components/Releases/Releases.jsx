785343645e0f8bb5566231ea9e5d119bf546f1c5
Sami <samisamman26@yahoo.com>
2024-09-28 20:38:31 -0400

    adding navigate functionality to new releases

--- react-vite/src/components/Releases/Releases.jsx
+++ react-vite/src/components/Releases/Releases.jsx
@@ -2,9 +2,11 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import './Releases.css'
 import { getAllDJThunk } from "../../redux/djs"
+import { useNavigate } from "react-router-dom"
 
 export default function Releases() {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     useEffect(()=> {
         getAllDJThunk()
     })
