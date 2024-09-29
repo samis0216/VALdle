c5b3168f4ec90d07bacee8179ffe129e38a8f63a
Sami <samisamman26@yahoo.com>
2024-09-28 20:37:23 -0400

    response component setup

---
+++ react-vite/src/components/Releases/Releases.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import './Releases.css'
+
+export default function Releases() {
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <p>Hot Re;eases This Week</p>
+        </div>
+    )
+}
