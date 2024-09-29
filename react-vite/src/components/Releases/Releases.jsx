865488a8f2b67dd76478c9b822ebbd39f851fd2a
Sami <samisamman26@yahoo.com>
2024-09-28 20:37:57 -0400

    setup useffect

--- react-vite/src/components/Releases/Releases.jsx
+++ react-vite/src/components/Releases/Releases.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import './Releases.css'
+import { getAllDJThunk } from "../../redux/djs"
 
 export default function Releases() {
     const dispatch = useDispatch()
+    useEffect(()=> {
+        getAllDJThunk()
+    })
     return (
         <div>
             <p>Hot Re;eases This Week</p>
