fc68a3d0e094284563c081982e78dc3a54219b7a
Sami <samisamman26@yahoo.com>
2024-11-30 19:07:31 -0400

    check for directory

--- react-vite/src/components/MP3_renamer/renamer.py
+++ react-vite/src/components/MP3_renamer/renamer.py
@@ -1,4 +1,10 @@
 import os
 
-def renamerMP3():
+def renamerMP3(djSongs):
+    try:
+        if not os.path.exists(djSongs):
+            print(f"The directory '{djSongs}' does not exist.")
+            return
+    except:
+        return
     return
