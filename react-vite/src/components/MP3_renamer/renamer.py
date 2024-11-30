16be56120f1b826f01899457dc22bd0088e59892
Sami <samisamman26@yahoo.com>
2024-11-30 19:08:36 -0400

    actual change logic implemented

--- react-vite/src/components/MP3_renamer/renamer.py
+++ react-vite/src/components/MP3_renamer/renamer.py
@@ -6,5 +6,15 @@ def renamerMP3(djSongs):
             print(f"The directory '{djSongs}' does not exist.")
             return
     except:
+        for filename in os.listdir(djSongs):
+            if filename.endswith(".mp3"):
+                old_path = os.path.join(djSongs, filename)
+                name, ext = os.path.splitext(filename)
+                new_name = f"{name}1{ext}"
+                new_path = os.path.join(djSongs, new_name)
+                os.rename(old_path, new_path)
+                print(f"Renamed: {filename} -> {new_name}")
+
+        print("Renaming completed.")
         return
     return
