28810f77fd38e8823c98d999d0bac236153759b2
samis0216 <samisamman26@yahoo.com>
2024-12-28 16:28:24 -0800

    created board method

--- app/api/chess_class.py
+++ app/api/chess_class.py
@@ -3,6 +3,18 @@ class ChessGame:
         self.board = self.create_board()
         self.current_turn = 'white'
 
+    def create_board(self):
+        board = [
+            ["r", "n", "b", "q", "k", "b", "n", "r"],
+            ["p", "p", "p", "p", "p", "p", "p", "p"],
+            [" "] * 8,
+            [" "] * 8,
+            [" "] * 8,
+            [" "] * 8,
+            ["P", "P", "P", "P", "P", "P", "P", "P"],
+            ["R", "N", "B", "Q", "K", "B", "N", "R"],
+        ]
+        return board
 
 if __name__ == "__main__":
     game = ChessGame()
