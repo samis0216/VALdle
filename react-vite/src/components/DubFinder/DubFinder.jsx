be26311e31a9de2d85e28ded01a270e4596e3b2c
Sami <samisamman26@yahoo.com>
2025-01-25 12:13:00 -0400

    added useStates

--- react-vite/src/components/DubFinder/DubFinder.jsx
+++ react-vite/src/components/DubFinder/DubFinder.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 
 const App = () => {
+    const [fileContent, setFileContent] = useState(null);
+    const [result, setResult] = useState(null);
 
+    return (
+        <h1>Hi</h1>
+    )
 }
 
 export default App;
