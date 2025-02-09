d1e5d138287379afd1f3d2de0e0df04bb176252a
Sami <samisamman26@yahoo.com>
2025-02-09 08:12:39 -0400

    added use effect

--- react-vite/src/components/Banner/Banner.jsx
+++ react-vite/src/components/Banner/Banner.jsx
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const FaceCardsBanner = ({ faceCards }) => {
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % faceCards.length);
+        }, 3000)
+        return () => clearInterval(interval);
+    }, [faceCards.length]);
 }
