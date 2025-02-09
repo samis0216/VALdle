958f02f08e95391a7391d7633cf39438ffaa3cf6
Sami <samisamman26@yahoo.com>
2025-02-09 08:13:23 -0400

    added return statement and styling

--- react-vite/src/components/Banner/Banner.jsx
+++ react-vite/src/components/Banner/Banner.jsx
@@ -10,4 +10,21 @@ const FaceCardsBanner = ({ faceCards }) => {
         }, 3000)
         return () => clearInterval(interval);
     }, [faceCards.length]);
+
+    return (
+        <div className="relative w-full h-64 overflow-hidden flex justify-center items-center">
+            <AnimatePresence>
+                <motion.img
+                    key={faceCards[index]}
+                    src={faceCards[index]}
+                    alt="Face Card"
+                    className="absolute w-full h-full object-cover rounded-2xl shadow-lg"
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.5 }}
+                />
+            </AnimatePresence>
+        </div>
+    );
 }
