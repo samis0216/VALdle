af4be98189f0447f38276bc9cdc9a4acce6276e7
Sami <samisamman26@yahoo.com>
2025-02-09 08:11:49 -0400

    banner imports

---
+++ react-vite/src/components/Banner/Banner.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const FaceCardsBanner = ({ faceCards }) => {
+  const [index, setIndex] = useState(0);
+
+}
